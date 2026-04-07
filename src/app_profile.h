db9726969416a97c313730e2b3ff917205741f4a
WearyConcern1165 <benganzafenix@gmail.com>
2026-04-07 12:29:28 +0200

    Delete src directory

--- src/app_profile.h
+++
@@ -1,6 +0,0 @@
-#pragma once
-
-// Load and apply environment variables from a profile file
-// Selects profile based on process name or EXYNOSTOOLS_APP_PROFILE
-void xeno_app_profile_apply(void);
-
